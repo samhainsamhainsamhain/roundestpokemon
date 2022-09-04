94b543188395c1b2d40225a53fa225f1cc2375a1
Samhain <rikalashnikov@gmail.com>
2022-09-04 19:14:32 +0300

    basic ui structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,15 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+        <div className="p-2" />
+        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+          <div className="w-16 h-16 bg-red-200"></div>
+          <div className="p-8">vs</div>
+          <div className="w-16 h-16 bg-blue-200"></div>
+        </div>
+      </div>
     </>
   );
 };
