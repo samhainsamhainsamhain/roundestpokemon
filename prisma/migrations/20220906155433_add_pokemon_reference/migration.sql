6b4394a0e9a2d88e54baa1a5e6708ed476b2e5b8
Samhain <rikalashnikov@gmail.com>
2022-09-06 21:20:21 +0300

    try to fix migration to planetscale

--- prisma/migrations/20220906155433_add_pokemon_reference/migration.sql
+++ prisma/migrations/20220906155433_add_pokemon_reference/migration.sql
@@ -1,11 +1,9 @@
 /*
   Warnings:
-
   - You are about to drop the column `votedAgainst` on the `Vote` table. All the data in the column will be lost.
   - You are about to drop the column `votedFor` on the `Vote` table. All the data in the column will be lost.
   - Added the required column `votedAgainstId` to the `Vote` table without a default value. This is not possible if the table is not empty.
   - Added the required column `votedForId` to the `Vote` table without a default value. This is not possible if the table is not empty.
-
 */
 -- AlterTable
 ALTER TABLE `Vote` DROP COLUMN `votedAgainst`,
--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,32 +1,36 @@
-import * as trpc from '@trpc/server';
-import { z } from 'zod';
-
-import {prisma} from '@/backend/utils/prisma'; 
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { prisma } from "@/backend/utils/prisma";
 
 export const appRouter = trpc
-  .router().query("get-pokemon-by-id", {
-    input: z.object({id: z.number()}),
-    async resolve({input}) {
-      const pokemon = await prisma.pokemon.findFirst({ where: {id: input.id}})
+  .router()
+  .query("get-pokemon-by-id", {
+    input: z.object({ id: z.number() }),
+    async resolve({ input }) {
+      const pokemon = await prisma.pokemon.findFirst({
+        where: { id: input.id },
+      });
 
-      if(!pokemon) throw new Error("pokemon doesn't exist")
+      if (!pokemon) throw new Error("lol doesn't exist");
 
-      return pokemon
-    }
-  }).mutation("cast-vote", {
+      return pokemon;
+    },
+  })
+  .mutation("cast-vote", {
     input: z.object({
       votedFor: z.number(),
       votedAgainst: z.number(),
     }),
-    async resolve({input}) {
+    async resolve({ input }) {
       const voteInDb = await prisma.vote.create({
         data: {
           votedAgainstId: input.votedAgainst,
-          votedForId: input.votedFor
-        }
-      })
-      return {success: true, vote: voteInDb}
-    }
-  })
+          votedForId: input.votedFor,
+        },
+      });
+      return { success: true, vote: voteInDb };
+    },
+  });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
