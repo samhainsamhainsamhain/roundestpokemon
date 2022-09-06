d8d43f732e715831337c5c9d810cc6a6e5b7c051
Samhain <rikalashnikov@gmail.com>
2022-09-06 19:32:58 +0300

    starting move to internal data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,17 +1,17 @@
 import * as trpc from '@trpc/server';
 import { z } from 'zod';
 
-import { PokemonClient } from 'pokenode-ts';
 import {prisma} from '@/backend/utils/prisma'; 
 
 export const appRouter = trpc
   .router().query("get-pokemon-by-id", {
     input: z.object({id: z.number()}),
     async resolve({input}) {
-      const pokeApiConnection = new PokemonClient();
+      const pokemon = await prisma.pokemon.findFirst({ where: {id: input.id}})
 
-      const pokemon = await pokeApiConnection.getPokemonById(input.id)      
-      return {name: pokemon.name, sprites: pokemon.sprites}
+      if(!pokemon) throw new Error("pokemon doesn't exist")
+
+      return pokemon
     }
   }).mutation("cast-vote", {
     input: z.object({
