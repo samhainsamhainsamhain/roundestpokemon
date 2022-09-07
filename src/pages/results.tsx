cda77a9cddc42426eb683849d0e59378c1de29e8
Samhain <rikalashnikov@gmail.com>
2022-09-07 11:27:16 +0300

    add percent round count to results page

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -25,18 +25,29 @@ const getPokemonInOrder = async () => {
 
 type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
 
+const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
+  const { VoteFor, VoteAgainst } = pokemon._count;
+  if (VoteFor + VoteAgainst === 0) {
+    return 0;
+  }
+  return (VoteFor / (VoteFor + VoteAgainst)) * 100;
+};
+
 const PokemonListing: React.FC<{
   pokemon: PokemonQueryResult[number];
-}> = (props) => {
+}> = ({ pokemon }) => {
   return (
-    <div className="flex border-b p-4">
-      <Image
-        src={props.pokemon.spriteUrl}
-        width={64}
-        height={64}
-        layout={'fixed'}
-      />
-      <div className="capitalize">{props.pokemon.name}</div>
+    <div className="flex border-b p-4 justify-between items-center">
+      <div className="flex items-center">
+        <Image
+          src={pokemon.spriteUrl}
+          width={64}
+          height={64}
+          layout={'fixed'}
+        />
+        <div className="capitalize pr-2">{pokemon.name}</div>
+      </div>
+      <div className="pr-4">{generateCountPercent(pokemon) + '%'}</div>
     </div>
   );
 };
